0bdf383872e90475491bc62fac36ddca2321e867
zhimingyang25 <129520300+zhimingyang25@users.noreply.github.com>
2025-07-03 14:57:37 +0800

    Update and rename pcbeta.ts to baidao.ts

--- server/sources/pcbeta.ts
+++
@@ -1,4 +0,0 @@
-export default defineSource({
-  "pcbeta-windows11": defineRSSSource("https://bbs.pcbeta.com/forum.php?mod=rss&fid=563&auth=0"),
-  "pcbeta-windows": defineRSSSource("https://bbs.pcbeta.com/forum.php?mod=rss&fid=521&auth=0"),
-})
